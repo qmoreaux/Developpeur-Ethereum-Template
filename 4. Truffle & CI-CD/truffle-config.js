3f96aa2e6c01c89509120f58010ad7c6ce421590
Quentin Moreaux <quentin@cblue.fr>
2023-02-10 16:13:11 +0100

    Adding goerli network

--- 4. Truffle & CI-CD/truffle-config.js
+++ 4. Truffle & CI-CD/truffle-config.js
@@ -1,4 +1,5 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
 
 module.exports = {
     networks: {
@@ -6,6 +7,15 @@ module.exports = {
             host: '127.0.0.1', // Localhost (default: none)
             port: 8545, // Standard Ethereum port (default: none)
             network_id: '*' // Any network (default: none)
+        },
+        goerli: {
+            provider: function () {
+                return new HDWalletProvider({
+                    mnemonic: { phrase: `${process.env.MNEMONIC}` },
+                    providerOrUrl: `https://goerli.infura.io/v3/${process.env.INFURA_ID}`
+                });
+            },
+            network_id: 5
         }
     },
     mocha: {},
