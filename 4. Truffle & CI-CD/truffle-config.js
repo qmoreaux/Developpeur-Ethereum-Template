dd2e34f02f63a7eec0e49a1ea82a868df1d59d36
Quentin Moreaux <quentin@cblue.fr>
2023-02-10 18:24:19 +0100

    Add Mumbai network

--- 4. Truffle & CI-CD/truffle-config.js
+++ 4. Truffle & CI-CD/truffle-config.js
@@ -16,6 +16,15 @@ module.exports = {
                 });
             },
             network_id: 5
+        },
+        mumbai: {
+            provider: function () {
+                return new HDWalletProvider({
+                    mnemonic: { phrase: `${process.env.MNEMONIC}` },
+                    providerOrUrl: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_ID}`
+                });
+            },
+            network_id: 80001
         }
     },
     mocha: {},
