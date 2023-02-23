15da99bee367b048bc2b7c022f3cef31668554e2
Quentin Moreaux <quentin@cblue.fr>
2023-02-23 23:18:24 +0100

    Add coverage to README

--- Hardhat/hardhat.config.js
+++ Hardhat/hardhat.config.js
@@ -1,6 +1,9 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('@nomiclabs/hardhat-web3');
+require('@nomiclabs/hardhat-truffle5');
+require('solidity-coverage');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+    solidity: '0.8.18'
 };
