4541a89e5c713b51933366e07a74ce40de1a1207
Quentin Moreaux <quentin@cblue.fr>
2023-02-23 17:24:57 +0100

    Clean truffle config and add eth-gas-reporter

--- Projet_2/truffle-config.js
+++ Projet_2/truffle-config.js
@@ -8,26 +8,15 @@ module.exports = {
             port: 8545, // Standard Ethereum port (default: none)
             network_id: '*' // Any network (default: none)
         },
-        goerli: {
-            provider: function () {
-                return new HDWalletProvider({
-                    mnemonic: { phrase: `${process.env.MNEMONIC}` },
-                    providerOrUrl: `https://goerli.infura.io/v3/${process.env.INFURA_ID}`
-                });
-            },
-            network_id: 5
-        },
-        mumbai: {
-            provider: function () {
-                return new HDWalletProvider({
-                    mnemonic: { phrase: `${process.env.MNEMONIC}` },
-                    providerOrUrl: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_ID}`
-                });
-            },
-            network_id: 80001
+    },
+    mocha: {
+        reporter: 'eth-gas-reporter',
+        reporterOptions: {
+            gasPrice: 1,
+            token: 'ETH',
+            showTimeSpent: true
         }
     },
-    mocha: {},
     compilers: {
         solc: {
             version: '0.8.18', // Fetch exact version from solc-bin (default: truffle's version)
