cf449fa940a52bb0fd88960c9022a2c19407b3d6
Quentin Moreaux <quentin@cblue.fr>
2023-03-05 21:55:47 +0100

    Clean

--- 6. Dapp/simple-storage-react/truffle/truffle-config.js
+++ 6. Dapp/simple-storage-react/truffle/truffle-config.js
@@ -1,20 +1,33 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
 module.exports = {
-    contracts_build_directory: '../client/src/contracts',
     networks: {
         development: {
             host: '127.0.0.1', // Localhost (default: none)
             port: 8545, // Standard Ethereum port (default: none)
             network_id: '*' // Any network (default: none)
+        },
+        goerli: {
+            provider: function () {
+                return new HDWalletProvider({
+                    mnemonic: { phrase: `${process.env.MNEMONIC}` },
+                    providerOrUrl: `https://goerli.infura.io/v3/${process.env.INFURA_ID}`
+                });
+            },
+            network_id: 5
         }
     },
-    mocha: {
-        // timeout: 100000
-    },
-
-    // Configure your compilers
+    mocha: {},
     compilers: {
         solc: {
-            version: '0.8.18' // Fetch exact version from solc-bin (default: truffle's version)
+            version: '0.8.18', // Fetch exact version from solc-bin (default: truffle's version)
+            settings: {
+                optimizer: {
+                    enabled: false,
+                    runs: 200
+                }
+            }
         }
     }
 };
