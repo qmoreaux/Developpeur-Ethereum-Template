baad6e5e04e9d4fc9cd5e27a251a979cc82d5dd7
Quentin Moreaux <quentin@cblue.fr>
2023-03-05 21:48:38 +0100

    Add events

--- 6. Dapp/simple-storage-react/client/src/components/Demo/Contract.jsx
+++ 6. Dapp/simple-storage-react/client/src/components/Demo/Contract.jsx
@@ -1,25 +1,53 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
+import useEth from '../../contexts/EthContext/useEth';
 
 function Contract({ value, text }) {
     const spanEle = useRef(null);
+
+    const [EventValue, setEventValue] = useState('');
+    const [oldEvents, setOldEvents] = useState('');
+
+    const {
+        state: { contract }
+    } = useEth();
+
+    useEffect(() => {
+        (async function () {
+            let oldEvents = await contract.getPastEvents('valueChanged', {
+                fromBlock: 0,
+                toBlock: 'latest'
+            });
+            let oldies = [];
+            oldEvents.forEach((event) => {
+                oldies.push(event.returnValues._val);
+            });
+            setOldEvents(oldies);
+
+            await contract.events
+                .valueChanged({ fromBlock: 'earliest' })
+                .on('data', (event) => {
+                    let lesevents = event.returnValues._val;
+                    setEventValue(lesevents);
+                })
+                .on('changed', (changed) => console.log(changed))
+                .on('error', (err) => console.log(err))
+                .on('connected', (str) => console.log(str));
+        })();
+    }, [contract]);
 
     return (
         <code>
             {`contract SimpleStorage {
   uint256 value = `}
-
             <span className="secondary-color" ref={spanEle}>
                 <strong>{value}</strong>
             </span>
-
             {`;
 
     string greet = `}
-
             <span className="secondary-color" ref={spanEle}>
                 <strong>{text}</strong>
             </span>
-
             {`;
 
   function read() public view returns (uint256) {
@@ -29,9 +57,15 @@ function Contract({ value, text }) {
   function write(uint256 newValue) public {
     value = newValue;
   }
-}`}
+}
+ 
+  Events arriving: `}{' '}
+            {EventValue}{' '}
+            {`
+ 
+  Old events: `}{' '}
+            {oldEvents}
         </code>
     );
 }
-
 export default Contract;
--- 6. Dapp/simple-storage-react/truffle/contracts/SimpleStorage.sol
+++ 6. Dapp/simple-storage-react/truffle/contracts/SimpleStorage.sol
@@ -5,12 +5,16 @@ contract SimpleStorage {
   uint256 value;
   string greeter;
 
+  event valueChanged(uint _val);
+  event greetChanged(string _val);
+
   function read() public view returns (uint256) {
     return value;
   }
 
   function write(uint256 newValue) public {
     value = newValue;
+    emit valueChanged(newValue);
   }
   
   function greet() public view returns (string memory) {
@@ -19,6 +23,7 @@ contract SimpleStorage {
 
   function setGreeter(string calldata _greet) public {
     greeter = _greet;
+    emit greetChanged(_greet);
   }
   
 }
